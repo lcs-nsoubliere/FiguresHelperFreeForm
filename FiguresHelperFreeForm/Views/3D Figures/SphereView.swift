9316b6afa9efb07d54e5633439e7e2bafe2b14ed
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-12-08 19:13:52 -0500

    Started to make it so you can type the radius value for sphere *CODE NOT WORKING*  (only committing because laptop is going to IT)

--- FiguresHelperFreeForm/Views/3D Figures/SphereView.swift
+++ FiguresHelperFreeForm/Views/3D Figures/SphereView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 struct SphereView: View {
     
     // MARK: Stored properties
-    @State var radius = 10.0
+    // what ever the user adds to the text field
+    @State var providedRadius = ""
     
     // MARK: Computed properties
-    var surfaceArea: Double {
-        return 4 * Double.pi * radius * radius
+    
+    //Check the input given by the user
+    // If possible, return a double
+    //otherwise (bad input) return nil
+    
+    var radius: Double? {
+      
+        //Test on the provided input
+        //1. Ensure that we can simply change the input into a Double
+        //2. Ensure that the value as a Double is more then 0
+        //with a guard statement we list the things we wish to be true... provide an action to carry out when those
+        //conditions are not met.
+        
+        guard let radius = Double(providedRadius),
+              radius > 0
+        else {
+            //when we test our failed, we do not have a valid radius
+            return nil
+        }
+        //if we get here then we know our radius is good
+        return radius
     }
+   
+    // attempts to calculate the area
+    //and if cant... returns nil
+    var area: Double? {
+        //is the input actually a double, or is it Double?(might be nil)
+        
+        guard let radius = radius
+        else {
+            //we didnt have a valid radius, we cant calculate the area...
+            return nil
+        }
     
     var body: some View {
         ScrollView {
